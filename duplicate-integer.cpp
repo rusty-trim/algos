80e3b7ba32eee255e5ddae0b6241d543343e3ea2
rusty <rustytrim11@gmail.com>
2026-02-15 18:36:41 -0500

    duplicate-integer

---
+++ duplicate-integer.cpp
@@ -0,0 +1,39 @@
+#include <map>
+#include <vector>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
+bool hasDuplicate(vector<int> &nums)
+{
+    int i = 0;
+    map<int, int> hash;
+    
+    for(i = 0; i < nums.size(); i++)
+    {
+        if(hash.find(nums[i]) != hash.end())
+            return true;
+
+        hash[nums[i]] = i;
+    }
+
+    return false;
+}
+
+int main()
+{
+    int i = 0;
+    vector<int> nums;
+
+    srand(time(NULL));
+
+    for(i = 0; i < 5; i++)
+    {
+        nums.push_back(rand() % 10);
+    }
+
+    printf("Checking duplicate for numbers: {%d, %d, %d, %d, %d}\n", nums[0], nums[1], nums[2], nums[3], nums[4]);
+    printf("Duplicate %s\n", hasDuplicate(nums) ? "found" : "not found");
+}
